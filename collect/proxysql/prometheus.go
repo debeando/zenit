7896b9a4d314a88187f1ac6aae993c875c2a0cbc
Nicola Strappazzon C <nicola@nicola.local>
2018-06-12 12:54:42 +0200

    Add quote on values.

--- collect/proxysql/prometheus.go
+++ collect/proxysql/prometheus.go
@@ -10,14 +10,14 @@ func PrometheusExport() {
 
   for i := range(stats.Items) {
     path := "proxysql_stats_queries"
-    path  = path + fmt.Sprintf("{schema=%s",  stats.Items[i].schema)
-    path  = path + fmt.Sprintf(",table=%s",   stats.Items[i].table)
-    path  = path + fmt.Sprintf(",command=%s", stats.Items[i].command)
+    path  = path + fmt.Sprintf("{schema=\"%s\"",  stats.Items[i].schema)
+    path  = path + fmt.Sprintf(",table=\"%s\"",   stats.Items[i].table)
+    path  = path + fmt.Sprintf(",command=\"%s\"", stats.Items[i].command)
 
-    fmt.Printf("%s,calc=count} %d\n", path, stats.Items[i].count)
-    fmt.Printf("%s,calc=time} %d\n", path, stats.Items[i].sum)
-    fmt.Printf("%s,calc=min} %d\n", path, stats.Items[i].min)
-    fmt.Printf("%s,calc=max} %d\n", path, stats.Items[i].max)
-    fmt.Printf("%s,calc=avg} %d\n", path, (stats.Items[i].sum / stats.Items[i].count))
+    fmt.Printf("%s,calc=\"count\"} %d\n", path, stats.Items[i].count)
+    fmt.Printf("%s,calc=\"time\"} %d\n", path, stats.Items[i].sum)
+    fmt.Printf("%s,calc=\"min\"} %d\n", path, stats.Items[i].min)
+    fmt.Printf("%s,calc=\"max\"} %d\n", path, stats.Items[i].max)
+    fmt.Printf("%s,calc=\"avg\"} %d\n", path, (stats.Items[i].sum / stats.Items[i].count))
   }
 }
