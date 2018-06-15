8824b8030346125d666e49da50d2f2810d433643
Nicola Strappazzon C <nicola@nicola.local>
2018-06-15 15:24:54 +0200

    Fix sysctl.go

--- collect/os/sysctl.go
+++ collect/os/sysctl.go
@@ -2,13 +2,33 @@ package os
 
 import (
   "fmt"
-  "gitlab.com/swapbyt3s/zenit/lib"
+  "io/ioutil"
+  "os"
+  "strconv"
+  "strings"
 )
 
 const NR_OPEN string  = "/proc/sys/fs/nr_open"
 const FILE_MAX string = "/proc/sys/fs/file-max"
 
 func GetSysLimits(){
-  fmt.Printf("os.linux_sysctl_fs.nr_open %d\n", lib.GetValueFromFile(NR_OPEN))
-  fmt.Printf("os.linux_sysctl_fs.file_max %d\n", lib.GetValueFromFile(FILE_MAX))
+  fmt.Printf("os.linux_sysctl_fs.nr_open %d\n", GetValueFromFile(NR_OPEN))
+  fmt.Printf("os.linux_sysctl_fs.file_max %d\n", GetValueFromFile(FILE_MAX))
+}
+
+func GetValueFromFile(path string) uint64 {
+  if _, err := os.Stat(path); err == nil {
+    content, err := ioutil.ReadFile(path)
+    if err != nil {
+      panic(err)
+    }
+
+    value, err := strconv.ParseUint(strings.Trim(string(content), "\n"), 10, 64)
+    if err != nil {
+      panic(err)
+    }
+    return value
+  }
+
+  return 0
 }
--- lib/file.go
+++ lib/file.go
@@ -1,29 +1,9 @@
 package lib
 
 import (
-  "io/ioutil"
-  "os"
   "os/exec"
-  "strconv"
   "syscall"
 )
-
-func GetValueFromFile(path string) uint64 {
-  if _, err := os.Stat(path); err == nil {
-    content, err := ioutil.ReadFile(path)
-    if err != nil {
-      panic(err)
-    }
-
-    value, err := strconv.ParseUint(string(content), 10, 64)
-    if err != nil {
-      panic(err)
-    }
-    return value
-  }
-
-  return 0
-}
 
 func PGrep(cmd string) int {
   _, err := exec.Command("/bin/bash", "-c", "/usr/bin/pgrep -x '" + cmd +"' > /dev/null").Output()
