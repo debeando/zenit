0f2748796a3e90a4103918878a3960fc37a13a54
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-06-15 22:14:29 +0200

    Refactoring collect proxysql.

--- collect/proxysql/prometheus.go
+++ collect/proxysql/prometheus.go
@@ -5,20 +5,20 @@ import (
 )
 
 func PrometheusExport() {
-  stats := LoadStats()
-  stats.Sort()
+  queries := LoadQueries()
+  queries.Sort()
 
-  for i := range(stats.Items) {
+  for i := range(queries.Items) {
     path := "proxysql_stats_queries"
-    path  = path + fmt.Sprintf("{schema=\"%s\"",  stats.GetSchema(i))
-    path  = path + fmt.Sprintf(",table=\"%s\"",   stats.GetTable(i))
-    path  = path + fmt.Sprintf(",command=\"%s\"", stats.GetCommand(i))
-    path  = path + fmt.Sprintf(",group=\"%s\"",   stats.GetGroup(i))
+    path  = path + fmt.Sprintf("{schema=\"%s\"",  queries.GetSchema(i))
+    path  = path + fmt.Sprintf(",table=\"%s\"",   queries.GetTable(i))
+    path  = path + fmt.Sprintf(",command=\"%s\"", queries.GetCommand(i))
+    path  = path + fmt.Sprintf(",group=\"%s\"",   queries.GetGroup(i))
 
-    fmt.Printf("%s,calc=\"count\"} %d\n", path, stats.GetCount(i))
-    fmt.Printf("%s,calc=\"time\"} %d\n", path, stats.GetSum(i))
-    fmt.Printf("%s,calc=\"min\"} %d\n", path, stats.GetMin(i))
-    fmt.Printf("%s,calc=\"max\"} %d\n", path, stats.GetMax(i))
-    fmt.Printf("%s,calc=\"avg\"} %d\n", path, stats.GetAvg(i))
+    fmt.Printf("%s,calc=\"count\"} %d\n", path, queries.GetCount(i))
+    fmt.Printf("%s,calc=\"time\"} %d\n", path, queries.GetSum(i))
+    fmt.Printf("%s,calc=\"min\"} %d\n", path, queries.GetMin(i))
+    fmt.Printf("%s,calc=\"max\"} %d\n", path, queries.GetMax(i))
+    fmt.Printf("%s,calc=\"avg\"} %d\n", path, queries.GetAvg(i))
   }
 }
