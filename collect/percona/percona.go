4aeb1b3f3f6e0fe3c641a350434833e9c01383a1
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-06-15 16:50:01 +0200

    Invert byte GetRunningProcess

--- collect/percona/percona.go
+++ collect/percona/percona.go
@@ -1,14 +1,5 @@
 package percona
 
-import (
-  "fmt"
-  "gitlab.com/swapbyt3s/zenit/lib"
-)
-
-func GetRunningProcess(){
-  fmt.Printf("os.process.mysqld %d\n", lib.PGrep("mysqld"))
-  fmt.Printf("os.process.proxysql %d\n", lib.PGrep("proxysql"))
-  fmt.Printf("os.process.pt_kill %d\n", lib.PGrep("pt-kill"))
-  fmt.Printf("os.process.pt_deadlock_logger %d\n", lib.PGrep("pt-deadlock-logger"))
-  fmt.Printf("os.process.pt_slave_delay %d\n", lib.PGrep("pt-slave-delay"))
+func Run() {
+  GetRunningProcess()
 }
--- collect/percona/process.go
+++ collect/percona/process.go
@@ -1,5 +1,14 @@
 package percona
 
-func Run() {
-  GetRunningProcess()
+import (
+  "fmt"
+  "gitlab.com/swapbyt3s/zenit/lib"
+)
+
+func GetRunningProcess(){
+  fmt.Printf("os.process.mysqld %d\n", lib.PGrep("mysqld") ^ 1)
+  fmt.Printf("os.process.proxysql %d\n", lib.PGrep("proxysql") ^ 1)
+  fmt.Printf("os.process.pt_kill %d\n", lib.PGrep("pt-kill") ^ 1)
+  fmt.Printf("os.process.pt_deadlock_logger %d\n", lib.PGrep("pt-deadlock-logger") ^ 1)
+  fmt.Printf("os.process.pt_slave_delay %d\n", lib.PGrep("pt-slave-delay") ^ 1)
 }
