88df9f54587e87d6d774c376f8532f6ad90cd479
Nicola Strappazzon C <nicola@nicola.local>
2018-07-12 17:31:04 +0200

    Fix zenit.mysql_audit_log output for clickhouse

--- output/clickhouse/clickhouse.go
+++ output/clickhouse/clickhouse.go
@@ -16,19 +16,19 @@ func SendMySQLAuditLog(event <-chan map[string]string) {
   go func() {
     for e := range event {
       sql := fmt.Sprintf("INSERT INTO zenit.mysql_audit_log " +
-                             "(_time,host_ip,name,command_class,connection_id,status,sqltext,user,host,os_user,ip) " +
-                             "VALUES ('%s',IPv4StringToNum('%s'),'%s',%s,%s,'%s','%s','%s','%s','%s')",
-                              common.ToDateTime(e["TIMESTAMP"]),
-                              ip_address,
-                              e["NAME"],
-                              e["COMMAND_CLASS"],
-                              e["CONNECTION_ID"],
-                              e["STATUS"],
-                              common.Escape(e["SQLTEXT"]),
-                              e["USER"],
-                              e["HOST"],
-                              e["OS_USER"],
-                              e["IP"])
+                         "(_time,host_ip,name,command_class,connection_id,status,sqltext,user,host,os_user,ip) " +
+                         "VALUES ('%s',IPv4StringToNum('%s'),'%s','%s',%s,%s,'%s','%s','%s','%s','%s')",
+                          common.ToDateTime(e["TIMESTAMP"]),
+                          ip_address,
+                          e["NAME"],
+                          e["COMMAND_CLASS"],
+                          e["CONNECTION_ID"],
+                          e["STATUS"],
+                          common.Escape(e["SQLTEXT"]),
+                          e["USER"],
+                          e["HOST"],
+                          e["OS_USER"],
+                          e["IP"])
 
       common.HTTPPost(config.DSN_CLICKHOUSE, sql)
     }
@@ -39,23 +39,23 @@ func SendMySQLSlowLog(event <-chan map[string]string) {
   go func() {
     for e := range event {
       sql := fmt.Sprintf("INSERT INTO zenit.mysql_slow_log " +
-                             "(_time,host_ip,bytes_sent,killed,last_errno,lock_time,query,query_time,rows_affected,rows_examined,rows_read,rows_sent,schema,thread_id,user_host) " +
-                             "VALUES (toDateTime(%s),IPv4StringToNum('%s'),%s,%s,%s,%s,'%s',%s,%s,%s,%s,%s,'%s',%s,'%s')",
-                              e["timestamp"],
-                              ip_address,
-                              e["bytes_sent"],
-                              e["killed"],
-                              e["last_errno"],
-                              e["lock_time"],
-                              common.Escape(e["query"]),
-                              e["query_time"],
-                              e["rows_affected"],
-                              e["rows_examined"],
-                              e["rows_read"],
-                              e["rows_sent"],
-                              e["schema"],
-                              e["thread_id"],
-                              e["user_host"])
+                         "(_time,host_ip,bytes_sent,killed,last_errno,lock_time,query,query_time,rows_affected,rows_examined,rows_read,rows_sent,schema,thread_id,user_host) " +
+                         "VALUES (toDateTime(%s),IPv4StringToNum('%s'),%s,%s,%s,%s,'%s',%s,%s,%s,%s,%s,'%s',%s,'%s')",
+                          e["timestamp"],
+                          ip_address,
+                          e["bytes_sent"],
+                          e["killed"],
+                          e["last_errno"],
+                          e["lock_time"],
+                          common.Escape(e["query"]),
+                          e["query_time"],
+                          e["rows_affected"],
+                          e["rows_examined"],
+                          e["rows_read"],
+                          e["rows_sent"],
+                          e["schema"],
+                          e["thread_id"],
+                          e["user_host"])
 
       common.HTTPPost(config.DSN_CLICKHOUSE, sql)
     }
