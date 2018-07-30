352c707dd8d254097fb5602a85ca2d50ab1d6588
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-07-30 21:15:47 +0200

    Add test for HTTPPost

---
+++ common/http_test.go
@@ -0,0 +1,20 @@
+package common_test
+
+import (
+  "net/http"
+  "net/http/httptest"
+  "testing"
+  "gitlab.com/swapbyt3s/zenit/common"
+)
+
+func TestHTTPPost(t *testing.T) {
+  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+  }))
+  defer ts.Close()
+
+  response := common.HTTPPost(ts.URL, "foo")
+  if ! response {
+    t.Errorf("HTTPPost() return an error.")
+  }
+}
