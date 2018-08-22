ab2a4199ff33a4f8a4a8aac20cbd9a51595c6aed
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-08-22 17:16:40 +0200

    Add more examples and coments for audit.sql

--- assets/examples/audit.sql
+++ assets/examples/audit.sql
@@ -1,5 +1,21 @@
 -- Query examples for ClickHouse on zenit.mysql_audit_log to analyze data.
+--
+-- Example 1: List the hosts and the last log inserted:
+SELECT
+    host_name,
+    MAX(_time)
+FROM zenit.mysql_audit_log
+GROUP BY host_name;
 
+-- Example 2: List the hosts with audit log count:
+SELECT
+    host_name,
+    count(*)
+FROM zenit.mysql_audit_log
+GROUP BY host_name
+ORDER BY count() DESC;
+
+-- Example 3: List the users with audit log count:
 SELECT DISTINCT
   user,
   count(*)
@@ -7,6 +23,7 @@ FROM zenit.mysql_audit_log
 GROUP BY user
 ORDER BY count() DESC;
 
+-- Example 4: List the hosts and users with audit log count:
 SELECT
   host_name,
   user,
@@ -15,6 +32,7 @@ FROM zenit.mysql_audit_log
 GROUP BY host_name, user
 ORDER BY count() DESC;
 
+-- Example 5: List the queries with execution errors:
 SELECT
   user,
   halfMD5(sqltext_digest),
@@ -26,13 +44,8 @@ where name = 'Query'
 GROUP BY user, halfMD5(sqltext_digest), sqltext_digest
 ORDER BY COUNT() DESC;
 
-SELECT DISTINCT
-  user,
-  sqltext,
-  _time
-FROM zenit.mysql_audit_log
-WHERE halfMD5(sqltext_digest) = 18043511469391647841 LIMIT 1;
-
+-- Example 6: List the specific hash of query digest to determine frecuency in
+--            specific day.
 SELECT
   toDate(_time),
   toHour(_time),
