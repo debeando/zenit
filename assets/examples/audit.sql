7debca0881e4cc9581d76ca2848b1fcad518ee83
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-08-22 10:10:11 +0200

    Add more examples for exploring audit log in clickhouse.

--- assets/examples/audit.sql
+++ assets/examples/audit.sql
@@ -1,12 +1,45 @@
 -- Query examples for ClickHouse on zenit.mysql_audit_log to analyze data.
 
 SELECT DISTINCT
-    user,
-    count(*)
+  user,
+  count(*)
 FROM zenit.mysql_audit_log
 GROUP BY user
 ORDER BY count() DESC;
 
-SELECT DISTINCT user, sqltext, _time
+SELECT
+  host_name,
+  user,
+  count(*)
+FROM zenit.mysql_audit_log
+GROUP BY host_name, user
+ORDER BY count() DESC;
+
+SELECT
+  user,
+  halfMD5(sqltext_digest),
+  sqltext_digest,
+  COUNT()
+FROM zenit.mysql_audit_log
+where name = 'Query'
+  AND command_class = 'error'
+GROUP BY user, halfMD5(sqltext_digest), sqltext_digest
+ORDER BY COUNT() DESC;
+
+SELECT DISTINCT
+  user,
+  sqltext,
+  _time
 FROM zenit.mysql_audit_log
 WHERE halfMD5(sqltext_digest) = 18043511469391647841 LIMIT 1;
+
+SELECT
+  toDate(_time),
+  toHour(_time),
+  count() AS c,
+  bar(c, 0, 12000, 20)
+FROM zenit.mysql_audit_log
+WHERE halfMD5(sqltext_digest) = 11527302759258400550
+  AND toDate(_time) = '2018-08-20'
+GROUP BY toDate(_time), toHour(_time)
+ORDER BY toDate(_time), toHour(_time);
