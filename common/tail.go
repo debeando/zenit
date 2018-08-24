641b239579e9262ea6e936260a06bad142c5916f
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-08-24 15:49:36 +0200

    Fix tail to start to end log file.

--- common/tail.go
+++ common/tail.go
@@ -1,13 +1,19 @@
 package common
 
 import (
+	"os"
+
 	"github.com/hpcloud/tail"
 )
 
 func Tail(filename string, out chan<- string) {
 	defer close(out)
 
-	t, _ := tail.TailFile(filename, tail.Config{Follow: true, Logger: tail.DiscardingLogger})
+	t, _ := tail.TailFile(filename, tail.Config{
+		Follow: true,
+		Logger: tail.DiscardingLogger,
+		Location: &tail.SeekInfo{0, os.SEEK_END},
+	})
 	for line := range t.Lines {
 		out <- line.Text
 	}
