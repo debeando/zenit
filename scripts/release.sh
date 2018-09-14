19313451d9091f269434f082fc6a4e71b0d79737
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-09-14 21:57:25 +0200

    Fix extraction for version number.

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,9 +14,10 @@ then
   exit 1
 fi
 
-TAG=$(cat config/config.go | grep VERSION | awk -F'"' '{$0=$2}1')
+TAG=$(cat config/config.go | grep "Version string =" | awk -F'"' '{$0=$2}1')
 
 git push --delete origin "v${TAG}"
+git tag --delete "v${TAG}"
 git tag "v${TAG}"
 git push --tags --force
 
