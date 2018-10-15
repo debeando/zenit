ffca1fd4da25857a1ed2bc7a5abba27171eb9949
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-10-15 17:37:45 +0200

    Add todo for input.mysql.check()

--- plugins/inputs/mysql/mysql.go
+++ plugins/inputs/mysql/mysql.go
@@ -6,5 +6,8 @@ import (
 )
 
 func Check() bool {
+        // @TODO: Verify in all config in JSON to find values with any ENABLE
+        // attribute to continue for this check.
+
 	return mysql.Check(config.File.MySQL.DSN, "MySQL")
 }
