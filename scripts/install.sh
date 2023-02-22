94044840f71f91f02e3476dc4c053b0dd230d535
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-02-22 22:43:15 +0100

    update names

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Apache License Version 2.0, January 2004
-# https://github.com/swapbyt3s/zenit/blob/master/LICENSE.md
+# https://github.com/debeando/zenit/blob/master/LICENSE.md
 
 set -e
 
@@ -21,7 +21,7 @@ if ! type "wget" > /dev/null; then
 fi
 
 FILE="zenit-linux_amd64.tar.gz"
-TAG=$(wget -qO- "https://api.github.com/repos/swapbyt3s/zenit/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+TAG=$(wget -qO- "https://api.github.com/repos/debeando/zenit/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 if [ -f /etc/systemd/system/zenit.service ]; then
   /usr/bin/zenit --uninstall
@@ -40,7 +40,7 @@ if [ -L /usr/bin/zenit ]; then
 fi
 
 if [ -n "${FILE}" ]; then
-  wget -qO- "https://github.com/swapbyt3s/zenit/releases/download/${TAG}/${FILE}" | tar xz -C /usr/local/bin/
+  wget -qO- "https://github.com/debeando/zenit/releases/download/${TAG}/${FILE}" | tar xz -C /usr/local/bin/
 fi
 
 if [ -f /usr/local/bin/zenit ]; then
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Apache License Version 2.0, January 2004
-# https://github.com/swapbyt3s/zenit/blob/master/LICENSE.md
+# https://github.com/debeando/zenit/blob/master/LICENSE.md
 
 if ! type "jq" > /dev/null; then
   echo "Require command tool, please install: jq"
@@ -21,9 +21,9 @@ git tag --delete "v${TAG}"
 git tag "v${TAG}"
 git push --tags --force
 
-curl --silent --output /dev/null --data "{\"tag_name\": \"v${TAG}\",\"target_commitish\": \"master\",\"name\": \"Pre Release v${TAG}\",\"body\": \"\",\"draft\": true,\"prerelease\": true}" "https://api.github.com/repos/swapbyt3s/zenit/releases?access_token=${GITHUB_TOKEN}"
+curl --silent --output /dev/null --data "{\"tag_name\": \"v${TAG}\",\"target_commitish\": \"master\",\"name\": \"Pre Release v${TAG}\",\"body\": \"\",\"draft\": true,\"prerelease\": true}" "https://api.github.com/repos/debeando/zenit/releases?access_token=${GITHUB_TOKEN}"
 
-ID=$(curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/swapbyt3s/zenit/releases | jq -r '.[0].id')
+ID=$(curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/debeando/zenit/releases | jq -r '.[0].id')
 
 rm -rf pkg/*
 
@@ -32,7 +32,7 @@ export BUILD_DATE
 
 go generate ./...
 mkdir -p pkg/linux_amd64/
-GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X github.com/swapbyt3s/zenit/command.BuildTime=${BUILD_DATE}" -o pkg/linux_amd64/zenit main.go
+GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X github.com/debeando/zenit/command.BuildTime=${BUILD_DATE}" -o pkg/linux_amd64/zenit main.go
 tar -czf pkg/linux_amd64/zenit-linux_amd64.tar.gz -C pkg/linux_amd64/ zenit
 
 curl -# \
@@ -42,6 +42,6 @@ curl -# \
      -H "Authorization:token ${GITHUB_TOKEN}" \
      -H "Content-Type:application/octet-stream" \
      --data-binary @pkg/linux_amd64/zenit-linux_amd64.tar.gz \
-     "https://uploads.github.com/repos/swapbyt3s/zenit/releases/${ID}/assets?name=zenit-linux_amd64.tar.gz"
+     "https://uploads.github.com/repos/debeando/zenit/releases/${ID}/assets?name=zenit-linux_amd64.tar.gz"
 
 echo -e "\r"
