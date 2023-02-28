7e311c2d3879ec033850aded3b886844598ea475
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-02-28 09:42:03 +0100

    fix path to find version on scripts/release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG=$(grep "const VERSION string" < main.go | awk -F'"' '{$0=$2}1')
+TAG=$(grep "const Number string" < command/version.go | awk -F'"' '{$0=$2}1')
 
 git push --delete origin "v${TAG}"
 git tag --delete "v${TAG}"
